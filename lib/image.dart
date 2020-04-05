44994d1ddfdb8aaf9b1bbb61e4315b56223c8774
Lu Tang <lutang@MacBook-Pro.hitronhub.home>
2020-04-05 11:03:09 -0700

    Initial commit

---
+++ lib/image.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+void image() => runApp(
+      MaterialApp(
+        home: Container(
+          child: Image.asset('images/IMG-0845.jpg'),
+        ),
+      ),
+    );
---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+void main() => runApp(
+      MaterialApp(
+        home: Scaffold(
+          backgroundColor: Colors.blue,
+          appBar: AppBar(
+            backgroundColor: Colors.blue[900],
+            title: Text(
+              'Ask me anything',
+            ),
+          ),
+          body: BallPage(),
+        ),
+      ),
+    );
+
+class BallPage extends StatefulWidget {
+  @override
+  _BallPageState createState() => _BallPageState();
+}
+
+class _BallPageState extends State<BallPage> {
+  var number = 1;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: FlatButton(
+          onPressed: () {
+            setState(() {
+              number = Random().nextInt(5) + 1;
+            });
+          },
+          child: Image.asset('images/ball$number.png'),
+        ),
+      ),
+    );
+  }
+}
